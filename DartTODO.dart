d3e2c6dae54998be1152c5e662d5249039429d59
Tomáš Čerevka <tomas@cerevka.cz>
2012-04-28 16:57:39 +0200

    Basic css styles

--- DartTODO.dart
+++ DartTODO.dart
@@ -11,14 +11,21 @@ class DartTODO {
   }
   
   void addTask() {
+    document.query('#messages').nodes.clear();
     InputElement input = document.query('#newTask');
     String task = input.value;
-    var element = new Element.html('<div>' + task + ' </div>');
-    var span = new Element.html('<span class="delete-task">X</span>');
-    span.on.click.add((e) => deleteTask(element));
-    element.nodes.add(span);
-    document.query('#tasks').nodes.add(element);
-    input.value = '';
+    
+    if (task == "") {
+      var message = new Element.html('<div>Task cannot be empty.</div>');
+      document.query('#messages').nodes.add(message);
+    } else {    
+      var element = new Element.html('<div>' + task + ' </div>');
+      var span = new Element.html('<span class="delete-task">X</span>');
+      span.on.click.add((e) => deleteTask(element));
+      element.nodes.add(span);
+      document.query('#tasks').nodes.add(element);
+      input.value = '';
+    }
   }
   
   void deleteTask(Element element) {
